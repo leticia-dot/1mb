1071a4043a1cd3701db17e139f5c06a57a2141b0
leticia-dot <guimaraes.luz.leticia@escola.pr.gov.br>
2024-11-05 11:41:34 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-
+<html>
+    <head>
+      <title> calculo da media</title>
+   <head>
+     <body>
+      <h1>= calculo da media de 1 aluno </h1>
+      <input type ="number" max = "100" id="nota_1"
+      placehoider = "insira a nota 1">
+     <button 10 =calcular onclick ="calcular()">
+     calcular
+     </button>
